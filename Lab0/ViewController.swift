b1da87b4f9532ea3865288559bcadf3f995f9ad9
Austin Harrison <derpywhale167@gmail.com>
2024-08-22 12:09:48 -0400

    Initial Commit

---
+++ Lab0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab0
+//
+//  Created by Austin Harrison on 8/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
